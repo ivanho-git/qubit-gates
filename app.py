c709ba60c80e56890c3f1316205a9fe8bd59df87
Ibhan Mukherjee <ibhaanm29@gmail.com>
2025-11-06 16:26:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1230,7 +1230,7 @@ with tab5:
     with col5:
         st.markdown("""
         <div class="profile-card">
-            <img src=IMG-20251106-WA0008.jpg class="profile-img">
+            <img src="blob:https://github.com/1a86ead8-c937-4189-be69-5eb11592629d" class="profile-img">
             <p class="profile-name">OM THAVARI</p>
             <p class="profile-role">Their Role (e.g., Project Manager)</p>
             <p class="profile-bio">
